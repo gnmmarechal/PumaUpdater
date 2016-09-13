716a730f36c66b50e6b6077cf8e8e53110ba1a0e
gnmmarechal <gnmpolicemata91@gmail.com>
2016-09-13 22:46:47 +0100

    Finished PumaUpdater bootstrapper

--- index.lua
+++ index.lua
@@ -1,9 +1,9 @@
---Corbenik/Skeith CFW Updater: RE - Client Script (CORE)
+--PumaUpdater - Puma33DS CFW Updater for the Nintendo 3DS || Bootstrapper Script (CORE)
 --Author: gnmmarechal
 --Runs on Lua Player Plus 3DS
 
 -- This script fetches the latest updater script and runs it. If the server-side script has a higher rel number, the CIA will also be updated.
-clientrel = 2
+clientrel = 1
 bootstrapver = "1.0.2"
 
 if not Network.isWifiEnabled() then --Checks for Wi-Fi
@@ -11,8 +11,8 @@ if not Network.isWifiEnabled() then --Checks for Wi-Fi
 end
 
 -- Set server script URL
-stableserverscripturl = "http://gs2012.xyz/3ds/corbenikupdaterre/index-server.lua"
-nightlyserverscripturl = "http://gs2012.xyz/3ds/corbenikupdaterre/cure-nightly.lua"
+stableserverscripturl = "http://gs2012.xyz/3ds/PumaUpdater/index-server.lua"
+nightlyserverscripturl = "http://gs2012.xyz/3ds/PumaUpdater/index-nightly.lua"
 
 --Set server CIA type (BGM/NOBGM)
 if System.doesFileExist("romfs:/bgm.wav") then
@@ -22,13 +22,13 @@ else
 end
 
 -- Create directories
-System.createDirectory("/corbenik-updater-re")
-System.createDirectory("/corbenik-updater-re/settings")
-System.createDirectory("/corbenik-updater-re/resources")
+System.createDirectory("/PumaUpdater")
+System.createDirectory("/PumaUpdater/settings")
+System.createDirectory("/PumaUpdater/resources")
 
 
 -- Check if user is in devmode or no (to either use index-server.lua or cure-nightly.lua)
-if System.doesFileExist("/corbenik-updater-re/settings/devmode") then
+if System.doesFileExist("/PumaUpdater/settings/devmode") then
 	serverscripturl = nightlyserverscripturl
 	devmode = 1
 else
@@ -36,11 +36,11 @@ else
 	devmode = 0
 end
 -- Download server script
-if System.doesFileExist("/corbenik-updater-re/cure.lua") then
-	System.deleteFile("/corbenik-updater-re/cure.lua")
+if System.doesFileExist("/PumaUpdater/cure.lua") then
+	System.deleteFile("/PumaUpdater/cure.lua")
 end
-Network.downloadFile(serverscripturl, "/corbenik-updater-re/cure.lua")
+Network.downloadFile(serverscripturl, "/PumaUpdater/cure.lua")
 
 -- Run server script
-dofile("/corbenik-updater-re/cure.lua")
+dofile("/PumaUpdater/cure.lua")
 System.exit()
